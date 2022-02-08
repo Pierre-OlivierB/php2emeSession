9c4169528f48b4c57f46a8f9c365ad6fa9c8cd7f
BAUDINO PIERRE <PIE.BAUDINO@pedago.mermoz.edu>
2022-02-08 15:11:18 +0100

    ajout shut down page 2

--- page2s.php
+++ page2s.php
@@ -1,22 +1,31 @@
 <?php 
 // Ouvrir/réactiver la session. 
 session_start(); 
+// Effacer toutes les informations de session. 
+$_SESSION = array(); 
+// Supprimer le cookie de session (si utilisé). 
+// Le cookie porte le nom de la variable qui stocke  
+// l’identifiant de session. 
+if (isset($_COOKIE[session_name()])) { 
+  setcookie(session_name(),'',time()-1,'/'); 
+} 
 // Détruire la session. 
 session_destroy(); 
 ?>
 <!DOCTYPE html>  
-    <html>  
-        <head> 
-            <meta charset="utf-8"/> 
-        </head> 
-        <body> 
-    <div> 
-        <b>Page 2</b><br/> 
+<html>  
+  <head> 
+    <meta charset="utf-8" />  
+    <title>Page 2</title> 
+    <title>Page 2</title>
+  </head> 
+  <body> <div> 
+    <b>Page 2</b><br/>
 <?php  
-echo 'Bonjour ', $_SESSION["nom"],'<br />'; 
-echo 'session_id() = ',session_id(),'<br />'; 
-?>
-<a href="page3.php">Page 3</a><br /> 
-     </div> 
+    echo 'Bonjour ',$_SESSION["nom"],'<br/>'; 
+    echo 'session_id() = ',session_id(),'<br/>'; 
+    ?>
+     <a href="page3.php">Page 3</a><br /> 
+    </div> 
   </body> 
 </html>
